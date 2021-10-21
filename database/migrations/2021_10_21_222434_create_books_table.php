092b809707c0e6258c622eda678a8a9172b466d4
hb168 <1281261856@qq.com>
2021-10-21 23:11:52 +0800

    books

---
+++ database/migrations/2021_10_21_222434_create_books_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->default('Unknown');
+            $table->timestamp('pubdate')->useCurrent();
+            $table->float('series_index')->default(1.0);
+            $table->string('isbn')->default('');
+            $table->string('path')->default('');
+            $table->boolean('has_cover')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
